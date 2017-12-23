54eb39284900285e6aa9816e66dd21905649eff1
steffen <stro889002@aol.com>
2017-12-23 13:08:49 +0100

    alle Dateien

--- prothesen.py
+++ prothesen.py
@@ -7,13 +7,13 @@ from mainwindow import Ui_MainWindow
 from achtung import Ui_Dialog
 
 
-class MainWindow(QMainWindow, Ui_MainWindow):  # Python 3 Mainwindow-Klasse
+class MainWindow(QMainWindow, Ui_MainWindow):  # Mainwindow-Klasse
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent=parent)
         self.setupUi(self)
 
 
-class achtung(QDialog, Ui_Dialog):  # Python 3 Dialog-Klasse
+class achtung(QDialog, Ui_Dialog):  # Dialog-Klasse
     def __init__(self):
         super().__init__()
         self.setupUi(self)
@@ -26,6 +26,23 @@ uwe = achtung()
 dic_prothesen = {}  # Dictionary für Formulardaten
 
 status = False  # Datensatzstatus False -> append, True -> update
+
+
+
+def init_dictionary():
+    global k_list, dic_prothesen
+    k_list = ["ID", "Patientennummer", "Prothesenart", "Prothesentyp", "proximal", "distal", "Seite", "Wechseleingriff",
+              "Praeop_roentgen", "Postop_roentgen", "Fraktur", "Planung", "Opdatum", "Operateur", "Assistenz",
+              "Op_zeiten", "Infektion", "Luxation", "Inklinationswinkel", "Trochanterabriss", "Fissuren",
+              "Thrombose/Embolie",
+              "Sterblichkeit", "Neurologie", "Dokumentation", "Memo", "knochenverankert", "periprothetisch",
+              "Reintervention",
+              "Abweichung", "CT", "ab_imp_art", "ab_imp_groesse", "ab_stab", "ab_blutung", "ab_präop", "ab_operation",
+              "ab_anaesthesie", "spaet_infekt", "Einweiser"]    # Key-Liste Datenbank
+    for it in k_list:
+        dic_prothesen.update({it:None})
+    print (dic_prothesen)
+
 
 
 def open_db():
@@ -72,22 +89,10 @@ ab_stab,ab_blutung,"ab_präop",ab_operation,ab_anaesthesie,spaet_infekt,"Einweis
     open_db()
     cur.execute(suche)
     lesen = cur.fetchone()
-    k_list = ["ID", "Patientennummer", "Prothesenart", "Prothesentyp", "proximal", "distal", "Seite", "Wechseleingriff",
-              "Praeop_roentgen", "Postop_roentgen", "Fraktur", "Planung", "Opdatum", "Operateur", "Assistenz",
-              "Op_zeiten", "Infektion", "Luxation", "Inklinationswinkel", "Trochanterabriss", "Fissuren",
-              "Thrombose/Embolie",
-              "Sterblichkeit", "Neurologie", "Dokumentation", "Memo", "knochenverankert", "periprothetisch",
-              "Reintervention",
-              "Abweichung", "CT", "ab_imp_art", "ab_imp_groesse", "ab_stab", "ab_blutung", "ab_präop", "ab_operation",
-              "ab_anaesthesie", "spaet_infekt", "Einweiser"]
-    v_list = list(lesen)
-    if len(dic_prothesen) < len(k_list):  # Datensatz noch nicht im Dictionary Formulardaten?
-        dic_prothesen = dict(zip(k_list, v_list))  # leeres Dictionary Formulardaten mit Datensatz füllen
-    else:
-        pos = 0
-        for it in k_list:
-            dic_prothesen.update({it: lesen[pos]})  # Dictionary Formulardaten mit Datensatz aktualisieren...
-            pos += 1
+    pos = 0
+    for it in k_list:
+        dic_prothesen.update({it: lesen[pos]})  # Dictionary Formulardaten mit Datensatz aktualisieren...
+        pos += 1
     close_db()
     aktualisiere_widgets()
 
@@ -103,8 +108,8 @@ def aktualisiere_widgets():  # Daten aus Dictionary ins Formular laden...
     w.checkBox_postop_roentgen.setChecked(dic_prothesen['Postop_roentgen'])
     w.checkBox_fraktur.setChecked(dic_prothesen['Fraktur'])
     w.checkBox_praeop_planung.setChecked(dic_prothesen['Planung'])
-    ds = str(dic_prothesen['Opdatum'])
-    yy = int(ds[0:4])
+    ds = str(dic_prothesen['Opdatum']) # Datums-String
+    yy = int(ds[0:4])   # Zerlegung ...
     mm = int(ds[5:7])
     dd = int(ds[8:10])
     w.dateEdit_opdatum.setDate(QDate(yy, mm, dd))
@@ -227,7 +232,8 @@ def change_prothesenart():  # Eingabemaske anpassen...
 
         w.checkBox_trochanterabriss.setVisible(False)  # Trochanterabriss aus
         w.checkBox_trochanterabriss.setCheckState(False)
-
+    init_comboBox_proximal()
+    init_comboBox_distal()
 
 def change_abweichung():  # Abweichung an und aus
     wglist = (  # Elemente der Eingabemaske in Liste laden
@@ -242,12 +248,12 @@ def change_abweichung():  # Abweichung an und aus
     if w.checkBox_abweichung.isChecked() == True:  # Eingabemaske für Abweichungen...
         for wg in wglist:  # einschalten
             wg.setVisible(True)
-        w.frame.setVisible(True)
+        w.groupBox_abweichung.setVisible(True)
     else:
         for wg in wglist:  # ausschalten
             wg.setVisible(False)
             wg.setCheckState(False)
-        w.frame.setVisible(False)
+        w.groupBox_abweichung.setVisible(False)
 
 
 def change_wechseleingriff():  # Änderung Wechseleingriff -> knochenverankert?
@@ -398,7 +404,7 @@ def set_start_default():  # alle Eingaben auf Standard stellen...
         it.setCheckState(checkBoxState[it])
 
 
-def save_state():  # Status in Dictionaries speichern
+def save_state():  # Status der Widgets in Dictionaries speichern
     lineEdits = w.findChildren(QLineEdit)
     global lineEditState
     lineEditState = {}
@@ -412,14 +418,14 @@ def save_state():  # Status in Dictionaries speichern
 
 
 def speichern():
-    # prüfen und speichern
-
+    # TODO prüfen und speichern
+    print (dic_prothesen)
     set_start_default()
     init_neuesFormular()
 
 
 def init_neuesFormular():  # neues Formular initialisieren
-
+    init_dictionary()
     init_lineEdit_patientennummer()
     init_comboBox_prothesenart()
     change_prothesenart()
@@ -439,7 +445,7 @@ w.checkBox_abweichung.stateChanged.connect(change_abweichung)  # Ereignis Abweic
 w.pushButton_suche.clicked.connect(suche_patientennummer)  # Ereignis Taste Suchen/Laden gedrückt
 w.comboBox_operateur.currentTextChanged.connect(change_operateur)  # Ereignis Wechsel Operateur
 w.comboBox_assistenz.currentTextChanged.connect(change_assistenz)  # Ereignis Wechsel Assistenz
-w.commandLinkButton_speichern.pressed.connect(speichern)  # Ereignis Taste Speichern gedrückt
+w.pushButton_speichern.pressed.connect(speichern)  # Ereignis Taste Speichern gedrückt
 w.comboBox_prothesenart.currentTextChanged.connect(change_prothesenart)  # Ereignis Wechsel Prothesenart
 w.lineEdit_patientennummer.textChanged.connect(change_patientennummer)  # Ereignis Änderung Patientennummer
 
