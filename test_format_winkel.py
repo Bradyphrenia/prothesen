f54c76822280da18afbe19b99a82649347c75453
steffen <stro889002@aol.com>
2018-12-23 12:18:51 +0100

    2018_12_23

--- test_format_winkel.py
+++ test_format_winkel.py
@@ -1,8 +1,10 @@
 #  Copyright (c) 2018. Steffen Troeger
 
 from unittest import TestCase
+from prothesen import format_winkel
 
 
 class TestFormat_winkel(TestCase):
     def test_format_winkel(self):
-        self.fail()
+        self.assertEqual(1, 1)
+        self.assertEqual(format_winkel('+2. '), '+ 2.0')
